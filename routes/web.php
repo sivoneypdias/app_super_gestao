5d441a7d318bf3ec65b2022a353b554e518da86f
Sivoney Pinto <sivoneypdias@gmail.com>
2021-11-16 07:55:27 -0300

    Routes - Implementando as rotas principal, sobre-nos e contato

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Página inicial';
+});
+
+Route::get('/sobre-nos', function () {
+    return 'Sobre nós';
+});
+
+Route::get('/contato', function () {
+    return 'Contato';
 });
