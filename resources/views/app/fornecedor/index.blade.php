28c13d48447b88d3f267c4681d1c70bb5392de61
Sivoney Pinto <sivoneypdias@gmail.com>
2021-11-21 18:14:32 -0300

    Implementação do controlador Fornecedor e inclusão de comentário blade na view

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,10 @@
+<h3>Fornecedor</h3>
+
+{{-- Comentário que será ignorado pelo interpretador do blade --}}
+
+{{ 'Impressão blade.' }}
+@php
+    // Comentário php 
+    echo '<br>';
+    echo 'Impressão em php.';
+@endphp
