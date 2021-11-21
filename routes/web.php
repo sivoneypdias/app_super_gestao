61a216ad566266a8e8b8d55d6bd0cb04a9e21a21
Sivoney Pinto <sivoneypdias@gmail.com>
2021-11-21 17:03:25 -0300

    Routes - Nomeando rotas

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,14 @@ use Illuminate\Support\Facades\Route;
 
 // versão 8.x do Laravel
 // Route::get('/',[\App\Http\Controllers\PrincipalController::class, 'principal']);
-Route::get('/','PrincipalController@principal');
-Route::get('/contato','ContatoController@contato');
-Route::get('/sobre-nos','SobreNosController@sobreNos');
+Route::get('/','PrincipalController@principal')->name('site.index');
+Route::get('/contato','ContatoController@contato')->name('site.contato');
+Route::get('/sobre-nos','SobreNosController@sobreNos')->name('site.sobrenos');
 Route::get('/login', function(){return 'Login'; });
 
 Route::prefix('/app')->group(function(){
-    Route::get('/clientes', function(){return 'Clientes'; });
-    Route::get('/fornecedores', function(){return 'Fornecedores'; });
-    Route::get('/produtos', function(){return 'Produtos'; });
+    Route::get('/clientes', function(){return 'Clientes'; })->name('app.clientes');
+    Route::get('/fornecedores', function(){return 'Fornecedores'; })->name('app.fornecedores');
+    Route::get('/produtos', function(){return 'Produtos'; })->name('app.produtos');
 });
 
