616b0461b88fa9dc13e8c35ed597d576aead67fa
Sivoney Pinto <sivoneypdias@gmail.com>
2021-11-23 18:34:48 -0300

    Adicionando assets as views (helper asset)

--- resources/views/site/sobre-nos.blade.php
+++ resources/views/site/sobre-nos.blade.php
@@ -225,12 +225,12 @@
             </div>  
         </div>
 
-        <div class="rodape">
+         <div class="rodape">
             <div class="redes-sociais">
                 <h2>Redes sociais</h2>
-                <img src="img/facebook.png">
-                <img src="img/linkedin.png">
-                <img src="img/youtube.png">
+                <img src="{{ asset('img/facebook.png') }}">
+                <img src="{{ asset('img/linkedin.png') }}">
+                <img src="{{ asset('img/youtube.png') }}">
             </div>
             <div class="area-contato">
                 <h2>Contato</h2>
@@ -240,7 +240,7 @@
             </div>
             <div class="localizacao">
                 <h2>Localização</h2>
-                <img src="img/mapa.png">
+                <img src="{{ asset('img/mapa.png') }}">
             </div>
         </div>
     </body>
