df2946d6862dfa8d965c4ab483cb22da4ce6decb
Sivoney Pinto <sivoneypdias@gmail.com>
2021-11-30 08:38:12 -0300

    Eloquent - Ajustando o nome da tabela no Model para um correto ORM

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
-    //
+    // https://laravel.com/docs/8.x/eloquent#table-names
+    protected $table = 'fornecedores';
 }
