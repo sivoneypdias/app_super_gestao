d6932e468e98172af557d8e239fa6176e6870a0a
Sivoney Pinto <sivoneypdias@gmail.com>
2021-11-30 10:16:15 -0300

    Eloquent - Inserindo registros com Creante e Fillable

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -8,4 +8,12 @@ class Fornecedor extends Model
 {
     // https://laravel.com/docs/8.x/eloquent#table-names
     protected $table = 'fornecedores';
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     * @see https://laravel.com/docs/7.x/eloquent#mass-assignment
+     */
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
