9b858f5f03f13b103d5870956d17e456202a2f2c
Sivoney Pinto <sivoneypdias@gmail.com>
2021-12-01 10:39:25 -0300

    Eloquent - Deletando registros com SoftDelete

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -3,9 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Fornecedor extends Model
 {
+
+    use SoftDeletes;
+    
     // https://laravel.com/docs/8.x/eloquent#table-names
     protected $table = 'fornecedores';
     
