c0b39bbe092581c8330a1f4793d1ac868fbe43a7
Sivoney Pinto <sivoneypdias@gmail.com>
2021-12-02 15:39:45 -0300

    Validações dos campos email e únicos | Persistindo dados e redirecionando a rota

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -18,13 +18,14 @@ class ContatoController extends Controller
     public function salvar(Request $request){
 
         $request->validate([            
-            'nome' => 'required|min:3|max:40',
+            'nome' => 'required|min:3|max:40|unique:site_contatos',
             'telefone' => 'required',
-            'email' => 'required',
-            'motivo_contato' => 'required',
+            'email' => 'email',
+            'motivo_contatos_id' => 'required',
             'mensagem' => 'required|max:2000'
         ]);
 
-        // SiteContato::create($request->all());
+        SiteContato::create($request->all());
+        return redirect()->route('site.index');
     }
 }
