b7134c43a9dc242e4a9f53af20de2901c7299e71
Sivoney Pinto <sivoneypdias@gmail.com>
2021-12-03 07:32:39 -0300

    Migrations - ajuste para adicionar fk motivo_contatos_id na tabela site_contatos do Postgres

--- database/migrations/2021_12_02_180652_alter_table_site_contatos_add_fk_motivo_contatos.php
+++ database/migrations/2021_12_02_180652_alter_table_site_contatos_add_fk_motivo_contatos.php
@@ -13,21 +13,12 @@ class AlterTableSiteContatosAddFkMotivoContatos extends Migration
      */
     public function up()
     {
-        // adicionando a coluna motivo_contatos_id
+        // adicionando a coluna motivo_contatos_id e a fk e removendo a coluna motivo_contato
         Schema::table('site_contatos', function (Blueprint $table) {
             $table->unsignedBigInteger('motivo_contatos_id');
-
-        });
-
-        // executar uma query SQL para copiar os valores do campo motivo_contato para motivo_contatos_id
-        DB::statement('update site_contatos set motivo_contatos_id = motivo_contato');
-
-        // criando fk e removendo a coluna motivo_contato
-        Schema::table('site_contatos', function (Blueprint $table) {
             $table->foreign('motivo_contatos_id')->references('id')->on('motivo_contatos');
             $table->dropColumn(['motivo_contato']);
         });
-
     }
 
     /**
@@ -37,18 +28,11 @@ class AlterTableSiteContatosAddFkMotivoContatos extends Migration
      */
     public function down()
     {
-        // criar a coluna motivo_contato e remover a fk
-        Schema::table('site_contatos', function (Blueprint $table) {            
-            $table->integer(['motivo_contato']);             
-            $table->dropForeign('site_contatos_motivo_contatos_id_foreign')->references('id')->on('motivo_contatos');            
-        });
-
-        // executar uma query SQL para copiar os valores do campo motivo_contatos_id para motivo_contato
-        DB::statement('update site_contatos set motivo_contato = motivo_contatos_id');
-
-         // removendo a coluna motivo_contatos_id
+         // removendo a coluna motivo_contatos_id, removendo a fk e adicionando a coluna motivo_contato
         Schema::table('site_contatos', function (Blueprint $table) {
             $table->dropColumn('motivo_contatos_id');
+            $table->dropForeign('site_contatos_motivo_contatos_id_foreign')->references('id')->on('motivo_contatos');
+            $table->integer(['motivo_contato']);
         });
     }
 }
