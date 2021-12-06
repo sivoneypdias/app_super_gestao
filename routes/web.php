85748953fe5996a71fd63c42a376d8174ddac9df
Sivoney Pinto <sivoneypdias@gmail.com>
2021-12-06 13:53:50 -0300

    Middleware - Apelidando middlewares e encadeamento de middlewares (criando um middleware de autenticação)

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,24 @@ use Illuminate\Support\Facades\Route;
 
 // versão 8.x do Laravel
 // Route::get('/',[\App\Http\Controllers\PrincipalController::class, 'principal']);
-Route::get('/','PrincipalController@principal')->name('site.index');
+Route::middleware('log.acesso')
+->get('/','PrincipalController@principal')
+->name('site.index');
 Route::get('/contato','ContatoController@contato')->name('site.contato');
 Route::post('/contato','ContatoController@salvar')->name('site.contato');
 Route::get('/sobre-nos','SobreNosController@sobreNos')->name('site.sobrenos');
 Route::get('/login', function(){return 'Login'; });
 
 Route::prefix('/app')->group(function(){
-    Route::get('/clientes', function(){return 'Clientes'; })->name('app.clientes');
-    Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
-    Route::get('/produtos', function(){return 'Produtos'; })->name('app.produtos');
+    Route::middleware('log.acesso', 'autenticacao')
+        ->get('/clientes', function(){return 'Clientes'; })
+        ->name('app.clientes');
+    Route::middleware('log.acesso', 'autenticacao')
+        ->get('/fornecedores', 'FornecedorController@index')
+        ->name('app.fornecedores');
+    Route::middleware('log.acesso', 'autenticacao')
+        ->get('/produtos', function(){return 'Produtos'; })
+        ->name('app.produtos');
 });
 
 Route::fallback(function(){
