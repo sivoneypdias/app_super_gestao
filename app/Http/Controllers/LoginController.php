1e5ca895eda9a088dcb541192b6c36144ff3133c
Sivoney Pinto <sivoneypdias@gmail.com>
2021-12-07 17:13:56 -0300

    Autenticação - Implementando o menu de opções da área protegida da aplicação e adicionando a função logout

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -50,11 +50,16 @@ class LoginController extends Controller
             $_SESSION['nome'] = $usuario->name;
             $_SESSION['email'] = $usuario->email;
 
-            return redirect()->route('app.clientes');
+            return redirect()->route('app.home');
         }
 
 
 
         return redirect()->route('site.login',['erro' => 1]);
+    }
+
+    public function sair(){
+        session_destroy();
+        return redirect()->route('site.index');
     }
 }
