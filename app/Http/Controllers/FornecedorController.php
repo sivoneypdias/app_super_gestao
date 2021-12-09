c75bba16e2d05fcb1bd7d1c03c63c09981b06359
Sivoney Pinto <sivoneypdias@gmail.com>
2021-12-09 08:58:10 -0300

    Implementando o cadastro de fornecedores parte 1

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -7,6 +7,16 @@ use Illuminate\Http\Request;
 class FornecedorController extends Controller
 {
     public function index(){
-        return view('app.fornecedor');
+        return view('app.fornecedor.index');
+    }
+
+    public function listar(){
+
+        return view('app.fornecedor.listar');
+    }
+
+    public function adicionar(){
+
+        return view('app.fornecedor.adicionar');
     }
 }
