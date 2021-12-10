d5518899d3823b84cf5f7aa497598e198be2972f
Sivoney Pinto <sivoneypdias@gmail.com>
2021-12-10 15:07:33 -0300

    Refatoração da edição de registros

---
+++ resources/views/app/produto/_components/form_create_edit.blade.php
@@ -0,0 +1,32 @@
+ @if(isset($produto->id))
+    <form method="post" action="{{ route('produto.update', ['produto' => $produto->id]) }}">
+        @csrf
+        @method('PUT')
+@else               
+    <form method="post" action="{{ route('produto.store') }}">
+        @csrf
+@endif    
+        <input type="text" name="nome" value="{{ $produto->nome ?? old('nome') }}" placeholder="Nome" class="borda-preta">
+            @if($errors->has('nome')) 
+                {{ $errors->first('nome') }}
+            @endif
+        <input type="text" name="descricao" value="{{ $produto->descricao ?? old('descricao') }}" placeholder="Descrição" class="borda-preta">
+            @if($errors->has('descricao')) 
+                {{ $errors->first('descricao') }}
+            @endif
+        <input type="text" name="peso" value="{{ $produto->peso ?? old('peso') }}" placeholder="Peso" class="borda-preta">
+            @if($errors->has('peso')) 
+                {{ $errors->first('peso') }}
+            @endif
+        <select name="unidade_id">
+            <option value="">Selecione</option>
+            @foreach ($unidades as $unidade)
+                <option value="{{ $unidade->id }}"  {{ $produto->unidade_id ?? old('unidade_id') == $unidade->id ? 'selected': '' }}>{{ $unidade->descricao }}</option>    
+            @endforeach
+        </select>
+            @if($errors->has('unidade_id')) 
+                {{ $errors->first('unidade_id') }}
+            @endif
+    
+        <button type="submit" class="borda-preta">{{ isset($produto->id) ? 'Alterar' : 'Cadastrar'}}</button>
+    </form>
--- resources/views/app/produto/create.blade.php
+++ resources/views/app/produto/create.blade.php
@@ -5,7 +5,7 @@
 @section('conteudo')
 
     <div class="conteudo-pagina">
-        <div class="titulo-pagina-2">
+        <div class="titulo-pagina-2">            
             <p>Adicionar Produto</p>
         </div>
 
@@ -18,33 +18,8 @@
         
         <div class="informacao-pagina">            
             <div style="width:30%; margin-left: auto; margin-right: auto;">
-                <form method="post" action="{{ route('produto.store') }}">
-                    @csrf
-                    
-                    <input type="text" name="nome" value="{{ $produto->nome ?? old('nome') }}" placeholder="Nome" class="borda-preta">
-                        @if($errors->has('nome')) 
-                            {{ $errors->first('nome') }}
-                        @endif
-                    <input type="text" name="descricao" value="{{ $produto->descricao ?? old('descricao') }}" placeholder="Descrição" class="borda-preta">
-                        @if($errors->has('descricao')) 
-                            {{ $errors->first('descricao') }}
-                        @endif
-                    <input type="text" name="peso" value="{{ $produto->peso ?? old('peso') }}" placeholder="Peso" class="borda-preta">
-                        @if($errors->has('peso')) 
-                            {{ $errors->first('peso') }}
-                        @endif
-                    <select name="unidade_id">
-                        <option value="">Selecione</option>
-                        @foreach ($unidades as $unidade)
-                            <option value="{{ $unidade->id }}"  {{ old('unidade_id') == $unidade->id ? 'selected': '' }}>{{ $unidade->descricao }}</option>    
-                        @endforeach
-                    </select>
-                        @if($errors->has('unidade_id')) 
-                            {{ $errors->first('unidade_id') }}
-                        @endif
-                
-                    <button type="submit" class="borda-preta">Cadastrar</button>
-                </form>
+               @component('app.produto._components.form_create_edit',['unidades' => $unidades])
+               @endcomponent           
             </div>
     </div>
 @endsection
