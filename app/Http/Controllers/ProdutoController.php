5895e53b78d67db6c108df0c67061aff7fad8aa2
Sivoney Pinto <sivoneypdias@gmail.com>
2021-12-10 10:45:19 -0300

    Implementando o cadastro de produtos parte 2 (create)

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Produto;
+use App\Unidade;
 use Illuminate\Http\Request;
 
 class ProdutoController extends Controller
@@ -28,6 +29,9 @@ class ProdutoController extends Controller
     public function create()
     {
         //
+        $unidades = Unidade::all();
+
+        return view('app.produto.create',['unidades' => $unidades]);
     }
 
     /**
