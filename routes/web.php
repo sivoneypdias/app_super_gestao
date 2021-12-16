91e439167c593f6a144c44295313e269d08bbc45
Sivoney Pinto <sivoneypdias@gmail.com>
2021-12-16 13:48:36 -0300

    Eloquent ORM N para N - Implementando os requisitos para o relacionamento (Cliente, Pedido e PedidoProduto)

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,7 @@ Route::post('/login','LoginController@autenticar')->name('site.login');
 
 Route::middleware('autenticacao:padrao')->prefix('/app')->group(function(){
     Route::get('/home', 'HomeController@index')->name('app.home');
-    Route::get('/sair', 'LoginController@sair')->name('app.sair');
-    Route::get('/cliente', 'ClienteController@index')->name('app.cliente');
+    Route::get('/sair', 'LoginController@sair')->name('app.sair');    
     Route::get('/fornecedor', 'FornecedorController@index')->name('app.fornecedor');
     Route::post('/fornecedor/listar', 'FornecedorController@listar')->name('app.fornecedor.listar');    
     Route::get('/fornecedor/listar', 'FornecedorController@listar')->name('app.fornecedor.listar');    
@@ -39,6 +38,10 @@ Route::middleware('autenticacao:padrao')->prefix('/app')->group(function(){
 
     // produto detalhes
     Route::resource('produto-detalhe', 'ProdutoDetalheController');
+        
+    Route::resource('cliente', 'ClienteController');
+    Route::resource('pedido', 'PedidoController');
+    Route::resource('pedido-produto', 'PedidoProdutoController');
 });
 
 Route::fallback(function(){
