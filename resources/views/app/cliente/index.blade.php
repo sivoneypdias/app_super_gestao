d10b7deb4d94538448ff6f6c4277da688a5d7c16
Sivoney Pinto <sivoneypdias@gmail.com>
2021-12-16 15:43:56 -0300

    Implementando a tela de listagem de clientes

---
+++ resources/views/app/cliente/index.blade.php
@@ -0,0 +1,57 @@
+@extends('app.layouts.basico')
+
+@section('titulo', 'Cliente')
+
+@section('conteudo')
+
+    <div class="conteudo-pagina">
+        <div class="titulo-pagina-2">
+            <p>Listagem de Clientes</p>
+        </div>
+
+        <div class="menu">
+            <ul>
+                <li><a href="{{ route('cliente.create') }}">Novo</a></li>
+                <li><a href="#">Consulta</a></li>           
+            </ul>
+        </div>
+        
+        <div class="informacao-pagina">
+            <div style="width:90%; margin-left: auto; margin-right: auto;">
+                <table border="1" width="100%">
+                <thead>
+                    <tr>
+                        <th>Nome</th>                       
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach ($clientes as $cliente)
+                    <tr>                        
+                        <td>{{ $cliente->nome }}</td>
+                        <td><a href="{{ route('cliente.show', ['cliente' => $cliente->id]) }}">Visualizar</a></td>
+                        <td>
+                             <form id="form_{{$cliente->id}}" method="post" action="{{ route('cliente.destroy', ['cliente' => $cliente->id]) }}">
+                                @csrf
+                                @method('DELETE')                                
+                                <a href="#" onclick="document.getElementById('form_{{$cliente->id}}').submit()">Excluir</a>
+                            </form>
+                        </td>
+                        <td><a href="{{ route('cliente.edit', ['cliente' => $cliente->id]) }}">Editar</a></td>                                                
+                    </tr>
+                @endforeach    
+                </tbody>
+            </table>
+                {{--  append to the query string of pagination links using the appends method. --}}
+                {{ $clientes->appends($request)->links() }}
+
+
+                {{--  https://laravel.com/docs/8.x/pagination#paginator-instance-methods  --}}
+                Exibindo {{ $clientes->count() }} clientes de {{ $clientes->total() }} (de {{ $clientes->firstItem() }} a {{ $clientes->lastItem() }})
+                
+
+            </div>
+    </div>
+@endsection
